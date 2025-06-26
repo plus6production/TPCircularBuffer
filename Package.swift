a6cfe95ec74b094f8aedee2e7686ea12a02fcc05
Charles Kiorpes <charlie@plus6production.com>
2025-06-26 15:03:13 -0400

    Change directory structure to try to get swiftpm to autogenerate a module map

--- Package.swift
+++ Package.swift
@@ -5,13 +5,15 @@ import PackageDescription
 let package = Package(
     name: "TPCircularBuffer",
     products: [
-        .library(name: "TPCircularBuffer", targets: ["CTPCircularBuffer"]),
+        .library(
+            name: "TPCircularBuffer",
+            targets: ["CTPCircularBuffer"]
+        ),
     ],
     targets: [
         .target(
             name: "CTPCircularBuffer",
-            path: "", publicHeadersPath: "",
             swiftSettings: [.interoperabilityMode(.Cxx)]
-        )
+        ),
     ]
 )
